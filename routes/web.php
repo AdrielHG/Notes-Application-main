c77d3c170e1aaaf1effdcad13a9085cc1dc08740
AdrielHG <101303036+AdrielHG@users.noreply.github.com>
2024-10-11 12:50:22 -0600

    now we start

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use \App\Http\Controllers\DataController;
+use App\Http\Controllers\PublicController;
+use App\Http\Controllers\CategoryController;
 
-Route::get('/', [DataController::class, 'index']);
+Route::get('/', [PublicController::class, 'welcome']);
+Route::resource('categories', CategoryController::class);
