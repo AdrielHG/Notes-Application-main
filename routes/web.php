db86aef661cd874ad3391d8305966d1530256cf4
AdrielHG <101303036+AdrielHG@users.noreply.github.com>
2024-11-02 22:33:40 -0600

    routes added again to project

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FolderController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\NoteController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,6 +19,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('categories', CategoryController::class);
+    Route::resource('folders', FolderController::class);
+    Route::resource('notes', NoteController::class);
+    Route::resource('users', UserController::class);
+
 });
 
 require __DIR__.'/auth.php';
