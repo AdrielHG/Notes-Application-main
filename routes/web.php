d3c2aa30cf63262203d3f257ce2adebd21b24169
AdrielHG <101303036+AdrielHG@users.noreply.github.com>
2024-11-02 22:27:40 -0600

    Breeze added to project

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,20 @@
 <?php
 
-use App\Http\Controllers\FolderController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\NoteController;
-use App\Http\Controllers\UserController;
-
-
-/* Route::get('/', [FolderController::class, 'index']);
-Route::get('/', [CategoryController::class, 'index']);
-Route::get('/', [NoteController::class, 'index']);
-Route::get('/', [UserController::class, 'index']); */
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('categories', CategoryController::class);
-Route::resource('folders', FolderController::class);
-Route::resource('notes', NoteController::class);
-Route::resource('users', UserController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
